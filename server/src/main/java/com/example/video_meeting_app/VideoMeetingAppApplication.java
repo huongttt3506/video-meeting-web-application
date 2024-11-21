0238861d339f59f3093958f92937c21582290341
huongttt3506 <huongttt.ueh@gmail.com>
2024-11-21 16:02:01 +0900

    Init project and push all files except .env

---
+++ server/src/main/java/com/example/video_meeting_app/VideoMeetingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.video_meeting_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoMeetingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoMeetingAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/video_meeting_app/VideoMeetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.video_meeting_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoMeetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
